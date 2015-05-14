298fcb2086e47d068c461b185d2a10bf69164206
xad2 <xadown0t@gmail.com>
2015-05-14 19:00:02 -0300

    v0.0.1

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+  
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+end
